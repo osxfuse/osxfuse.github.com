ae8d251db380c457dfc0aa2be45376b9ecce3826
Benjamin Fleischer <fleiben@gmail.com>
2011-09-23 16:38:56 +0200

    Use jQuery for outline effect of website title

---
+++ static/default.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".title").css("color", "white")
+    $(".title").FontEffect({
+        outline: true,
+        outlineColor1: "#698195",
+        outlineColor2: "#698195",
+        outlineWeight: 1
+    })
+});
